d71d98923c3622a5313ae42671f47c783376980f
Darius <dariuskt@gmail.com>
2018-12-30 17:22:26 +0000

    basic conection to local wifi

--- src/main.py
+++ src/main.py
@@ -1,9 +1,23 @@
-import os
 import time
 
+def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('wifi-name', 'wifi-pass')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
 
-while True:
-    time.sleep_ms(1000)
-    print('ping')
 
+def main():
+    do_connect()
+    while True:
+        time.sleep(1)
+        print('ping')
+
+
+main()
 
